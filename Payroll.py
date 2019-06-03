13b2969fb12bd1c155296c29bdebcfd18bf6b772
GiovanniCanarecci <50915561+GiovanniCanarecci@users.noreply.github.com>
2019-06-03 16:31:57 +0300

    Second commit: debugged!

--- Payroll.py
+++ Payroll.py
@@ -1,118 +1,91 @@
 #1) enter new employee (they have to be unique: so dictionary) -> Enter name
 #it enters the name and than comes back to the screen
 #2) calculate pay -> which employee (show the list) + enter #hours worked.
-#then display
-#gross pay
-#(-) Tax
-#Net pay
+#then display: gross pay, (-) Tax, Net pay
 #3) List employees in alphabetical order
-#Employee list
-#name, net pay
+#Employee list>: name, net pay
 #x) quit
 
-#assumption (7x24 is the maximum)
-#payrate: 10/h (minimum wage) #they may change later on
-#overtime: 15/h
-#OTime only after 40 hours
-#Tax rate: 18%
-#A SET OF LISTS!!!
+#A set of lists!
 employees= list()
 employeesDict= dict()
 quit=False
 userImput = None
 #Payrolls calculation variables
-Minimum=10
-OTime=15
-Taxperc=18
+#assumption (7x24 is the maximum #hours) 
 hours=0.0
 gross=0.0
 tax=0.0
 nett=0.0
-taxRate=0.18
-otRate=1.5
-regHours=40
-basicPay=10
+taxRate=0.18   #Tax rate: 18%
+otRate=1.5     #overtime: 15/h (only after regHours hours)
+regHours=40 
+basicPay=10    #payrate: 10/h (minimum wage)
 
+print('Hello!')
 while not quit: #quit == False:
-
-    print('Hello! Your choices are:')
+    print('Your choices are:')
     print('1 - Enter new employee')
     print('2 - Calculate pay')
     print('3 - List employees')
     print('x - Quit')
-    userInput = input('Please make your choice:') #IT RECEIVES ONLY STRINGS
-    print(userInput)
-    #1) enter new employee (they have to be unique: so dictionary) -> Enter name
-    #it enters the name and than comes back to the screen
+    userInput = input('Please make your choice: ') #IT RECEIVES ONLY STRINGS
+    #print(userInput)
+    #1) enter new employee (they have to be unique: so dictionary). It enters the name and than comes back to the screen.
     if userInput =='1':
         userInput = None
-        NewName = input('Enter name and surname separeted by a space, then press Enter:') #IT RECEIVES ONLY STRINGS
+        NewName = input('Enter name of the employee, then press Enter: ') #IT RECEIVES ONLY STRINGS
         employees.append([NewName,0])
-        employeesDict[NewName] =(0,0,0) #a tuple of three values
-        #print(NewName)
-    #2) calculate pay -> which employee (show the list) + enter #hours worked.
-    #then display
-    #gross pay
-    #(-) Tax
-    #Net pay
+        employeesDict[NewName] =(0,0,0) #a tuple of three values, to be filled later
+        #print(NewName)#for debugging
+    #2) calculate pay: ask which employee (show the list) + enter #hours worked. Then display: gross pay, (-) Tax, Net pay
     elif userInput =='2':
-        userInput = None
-        userInput =input('For which employee: ')
-        result = employeesDict.get(userInput,'No such employee')
-        if result != 'No such employee':
-            hours = float(input('Enter # hours wordes: '))
-            if hours > regHours:
-                gross=((hours-regHours)*otRate*basicPay)+ (regHours*basicPay)
-            else:
-                gross=hours*basicPay
-            tax=gross*taxRate
-            net=gross-tax
-            employeesDict[userInput] = (gross,tax,net)
-
         if len(employees) == 0:
-            print('Enter at least one employee first!')
+            print('First add at least one employee.')
         else:
-            print('Here there is the list of your employees. Choose one:')
-            for counter in (0,len(employees)):
+            userInput = None
+            print('Here there is the list of your employees. Choose one: ')
+            for counter in range(0,len(employees)):
+                #print(counter)#for debugging
                 listEmployees = '{0} - {1}'.format(counter, employees[counter][0]) #but it's a set and it must be in alphabetical order!
                 print(listEmployees)
-            InputEmployee = input('Choose the number of the employee whose pay you want to enter') #IT RECEIVES ONLY STRINGS
-            print(InputEmployee)
-            InputHours = input('Enter the number of hours of {0} for this week',employees[InputEmployee][0]) #IT RECEIVES ONLY STRINGS
-            print(InputHours)
-            #COMPUTATION
-            if InputHours >= 168:
-                print('He is cheating, he does not work that much! Now I am offended!')
-            if InputHours <= 40:
-                Payrate = InputHours*Minimum
-                Overtime =0
-            else:
-                Payrate = 40*Minimum
-                Overtime = (InputHours-40)*OTime
-            Gross = Payrate + Overtime
-            Taxation = Gross*(100-Taxperc)*100
-            Net = Gross - Taxation
-            print('Gross salary: {0} €',Gross)
-            print('Taxation: {0} €',Taxation)
-            print('Net salary: {0} €',Net)
-            employees[InputEmployee][1] = Net
-    #3) List employees in alphabetical order
-    #Employee list
-    #name, net pay
+                #print(len(employees))#for debugging
+            InputEmployee = input('Choose the number(!) of the employee whose pay you want to enter: ') #IT RECEIVES ONLY STRINGS
+            #print(InputEmployee)#for debugging
+            try:
+                InputEmployee_index = int(InputEmployee)
+                result = employeesDict.get(employees[InputEmployee_index][0],'No such employee')
+                #print(result)#for debugging
+                if result != 'No such employee':
+                    hours = float(input('Enter # hours worked this week by {}: '.format(employees[InputEmployee_index][0])))
+                    if hours <= 24*7:
+                        if hours > regHours:
+                            gross=((hours-regHours)*otRate*basicPay)+ (regHours*basicPay)
+                        else:
+                            gross=hours*basicPay
+                        tax=gross*taxRate
+                        net=gross-tax
+                        employeesDict[userInput] = (gross,tax,net)
+                        print('Gross salary: {0} €'.format(gross))
+                        print('Taxation: {0} €'.format(tax))
+                        print('Net salary: {0} €'.format(net))
+                        employees[InputEmployee_index][1] = net
+                    else:
+                        print('This is cheating! This amount of hours is impossible even for me (the computer)! Now I am offended!')
+                else:
+                    print('No such employee.')
+            except ValueError:
+                print("That's not an integer number!")
+    #3) List employees in alphabetical order. Employee list: name, net pay
     elif userInput =='3':
         userInput = None
         if len(employees) == 0:
             print('Enter at least one employee first!')
         else:
             print('Employee List')
-            nameList = employeesDict.keys()
-            nameList.sort()
-            for name in nameList:
-                print('{} -> {}'.format(name,employeesDict[name][2]))
-
-            employees.sort()
-            for countershow in (0,len(employees)):
-                print('{0}: {1} euro',employees[countershow][0],employees[countershow][1])
+            for counter in range(0,len(employees)):
+                listEmployees_net = '{0} -> {1} euro'.format(employees[counter][0], employees[counter][1]) #but it's a set and it must be in alphabetical order!
+                print(listEmployees_net)
     #x) quit
     elif userInput =='x':
         quit=True
